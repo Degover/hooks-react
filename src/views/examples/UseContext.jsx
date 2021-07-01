4e66ec20e1224c3ebf416980034c8b2b19ac5aa1
Degover <chaors2000@gmail.com>
2021-07-01 20:37:12 -0300

    Uso do contexto, aula 72

--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+import DataContext from '../../data/DataContext'
+
 const UseContext = (props) => {
+    const context = useContext(DataContext);
+
+    function addtNumber(delta) {
+        context.setState({
+            ...context.state,
+            number: context.state.number + delta
+        });
+    }
+
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
-                subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
-            />
+                subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!" />
+            <div className="center">
+                <span className="text">{context.state.text}</span>
+                <span className="text">{context.state.number}</span>
+
+                <div>
+                    <button className="btn" onClick={() => addtNumber(1)} >+1</button>
+                    <button className="btn" onClick={() => addtNumber(-1)} >-1</button>
+                </div>
+            </div>
         </div>
     )
 }
